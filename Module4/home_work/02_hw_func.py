8134b8cca1a620b0f09c9ba7eec412b547add478
Aleksey Dernovskiy <88583638+Dernovskiy@users.noreply.github.com>
2021-08-27 12:10:17 +0300

    Update 02_hw_func.py

--- Module4/home_work/02_hw_func.py
+++ Module4/home_work/02_hw_func.py
@@ -5,9 +5,21 @@
 # При решении задачи необходимо использовать функцию расстояния между двумя точками.
 
 def distance(x1, y1, x2, y2):
-    # TODO: тело, которое вы реализовали на практической работе
-    pass
+    # TODO: your code here
+    dist = ((x2 - x1)**2  + (y2 - y1)**2)**0.5
+    return dist
 
+def can_triangle(p1,p2,p3):
+    # TODO: your code here
+    AB = distance(*p1,*p2)
+    BC = distance(*p2,*p3)
+    AC = distance(*p1,*p3)
+    min = 'AB'
+    if AB > BC and AC > BC:
+     min = 'BC'
+    elif AB > AC: min = 'AC'
+    return AB, BC, AC, "Самый короткий отрезок: ", min
 
-# TODO: your code here
-print("Самый короткий отрезок:", ...)  # Выводим название отрезка, например “АС”.
+# Пример вызова функции
+print (can_triangle((10, 12), (14, 12), (12, 12)))
+print (can_triangle((6, 6), (7, 6), (5, 6)))
